49a85491f442def505594a32684d77cccd7fa291
hafizh abdillahh <hafizh.abdillah@flash-coffee.com>
2021-12-16 22:39:20 +0700

    ref: use useReducer on counterScreen

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,32 +1,39 @@
-import React, { useState } from "react"
-import { View, StyleSheet, Button, Text } from "react-native"
+import React, { useReducer } from "react";
+import { View, StyleSheet, Button, Text } from "react-native";
+const ACTION_INCREASE = "increase";
+const ACTION_DECREASE = "decrease";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increase":
+      return state + 1;
+    case "decrease":
+      return state - 1 < 0 ? 0 : state - 1;
+    default:
+      return state;
+  }
+};
 
 const CounterScreen = () => {
-    let [count, setCounter] = useState(0)
-    const doCount = (condition) => {
-        if (condition === "increase") {
-            count++;
-        } else {
-            count--;
-        }
-        if (count < 0) {
-            return;
-        }
-        setCounter(count)
-    }
-    return (
-        <View>
-            <Button title="Increse" onPress={() => doCount("increase")} />
-            <Button title="Decrease" onPress={() => doCount("decrease")} />
-            <Text style={styles.text}>Current Count: {count}</Text>
-        </View>
-    )
-}
+  const [state, dispatch] = useReducer(reducer, 0);
+  return (
+    <View>
+      <Button
+        title="Increase"
+        onPress={() => dispatch({ type: ACTION_INCREASE })}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => dispatch({ type: ACTION_DECREASE })}
+      />
+      <Text style={styles.text}>Current Count: {state}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    text: {
-        fontSize: 30
-    }
-})
+  text: {
+    fontSize: 30,
+  },
+});
 
-export default CounterScreen;   
+export default CounterScreen;
