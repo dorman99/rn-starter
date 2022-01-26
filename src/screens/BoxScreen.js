bbc5120dfb98cdf9318c746d000b9cbfa7f3bcec
Hafizh Abdillah <hafizh.abdillah@flash-coffee.com>
2022-01-27 00:36:47 +0700

    using parent wrapper

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -5,7 +5,9 @@ const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
       <View style={styles.textBoxOneStyle}></View>
-      <View style={styles.textBoxTwoStyle}></View>
+      <View style={styles.viewTwoParent}>
+        <View style={styles.textBoxTwoStyle}></View>
+      </View>
       <View style={styles.textBoxThreeStyle}></View>
     </View>
   );
@@ -24,10 +26,14 @@ const styles = StyleSheet.create({
     width: 75,
     backgroundColor: 'red'
   },
+  viewTwoParent: {
+    justifyContent: 'flex-end',
+    borderColor: 'black',
+    borderWidth: 2,
+  },
   textBoxTwoStyle: {
     height: 75,
     width: 75,
-    alignSelf: 'flex-end',
     backgroundColor: 'green'
   },  
   textBoxThreeStyle: {
